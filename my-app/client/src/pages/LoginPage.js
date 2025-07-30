2df631add5d668adbbbe253526c06308ad8dded3
Prasanth Devarapalli <116416695+PrashanthDev31@users.noreply.github.com>
2025-07-30 16:15:55 -0400

    Checking git for the first time, added a comment

--- my-app/client/src/pages/LoginPage.js
+++ my-app/client/src/pages/LoginPage.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import { useGoogleLogin } from '@react-oauth/google';
+ 
+//console
 
 const SpinnerIcon = ({ color = 'text-white' }) => (
     <svg className={`animate-spin -ml-1 mr-3 h-5 w-5 ${color}`} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
